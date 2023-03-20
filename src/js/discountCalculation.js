a3db0ce59312c41190b469803a21e8e6536e49a3
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2023-03-20 15:40:38 +0100

    fixing % calculation

--- src/js/discountCalculation.js
+++ src/js/discountCalculation.js
@@ -6,9 +6,19 @@
  */
 export const priceDiscount = (product) => {
  // Calculating discount price
- const fullPrice = product.price;
- const reducedPrice = product.discountedPrice;
- const discountPercentage = Math.trunc(100 * Math.abs((fullPrice - reducedPrice) / ((fullPrice + reducedPrice) / 2)));
+ const fullPrice = Math.round(product.price);
+ const reducedPrice = Math.round(product.discountedPrice);
+ const discountPercentage = Math.round(((fullPrice - reducedPrice) / fullPrice) * 100);
+
+ //  const discountPercentage = Math.trunc(100 * Math.abs((fullPrice - reducedPrice) / ((fullPrice + reducedPrice) / 2)));
+
+ console.log("Full price:", Math.round(product.price));
+ console.log("ReducedPrice:", Math.round(product.discountedPrice));
+ console.log("Discount %:", discountPercentage);
 
  return discountPercentage;
 };
+
+export function getPercentageIncrease(numA, numB) {
+ return ((numA - numB) / numB) * 100;
+}
