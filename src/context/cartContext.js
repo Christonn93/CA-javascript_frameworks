af4bcbf36cf94c56a6ca4f95d9765404b552ead8
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2023-03-20 09:50:41 +0100

    Removing useEffect
    Page will not build on netlify if there is errors in eslint.
    Removed useEffect because of this

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 import ApiHook from "../api/ApiHooks";
 
 export const CartContext = createContext({
