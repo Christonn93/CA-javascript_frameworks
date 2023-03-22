c017437310cc21ba1aaaae633419503aa849d6e4
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2023-03-22 17:38:00 +0100

    Starting work on form validation

--- src/components/Form/ContactForm.jsx
+++ src/components/Form/ContactForm.jsx
@@ -4,9 +4,19 @@ import React from "react";
 // Importing MUI
 import { Button, TextField, Box, Container } from "@mui/material";
 
-const ContactForm = () => {
- const validInput = false;
+// Function to check input data
+const nameIsValid = () => {};
 
+// Function to check input data
+const emailIsValid = () => {};
+
+// Function to check input data
+const subjectIsValid = () => {};
+
+// Function to check input data
+const messageIsValid = () => {};
+
+const ContactForm = () => {
  return (
   <Container align="center">
    <h1>Contact us</h1>
@@ -19,25 +29,25 @@ const ContactForm = () => {
     noValidate
     autoComplete="off"
    >
-    {validInput ? (
+    {nameIsValid ? (
      <TextField fullWidth required id="outlined-required" label="Full Name" helperText="Please enter your name" name="text" />
     ) : (
      <TextField fullWidth required id="outlined-error" label="Full Name" error helperText="Minimum number of characters is 3, required" />
     )}
 
-    {validInput ? (
+    {emailIsValid ? (
      <TextField fullWidth required id="outlined-required" label="Email" helperText="Please enter your Email" name="email" />
     ) : (
      <TextField fullWidth required id="outlined-required" label="Email" error helperText="Must be a valid email address, required" />
     )}
 
-    {validInput ? (
+    {subjectIsValid ? (
      <TextField fullWidth required id="outlined-required" label="Subject" helperText="Please enter a subject" name="text" />
     ) : (
      <TextField fullWidth required id="outlined-error" label="Subject" error helperText="Minimum number of characters is 3, required" />
     )}
 
-    {validInput ? (
+    {messageIsValid ? (
      <TextField fullWidth id="outlined-multiline-static" label="Message" multiline rows={4} helperText="Please enter your message" />
     ) : (
      <TextField fullWidth id="outlined-error" label="Message" multiline rows={4} error helperText="Incorrect entry." />
